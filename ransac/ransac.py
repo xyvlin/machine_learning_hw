66f2c4ff689433c4f1cfc2d8fe7dc3ceb35f6db6
xyvlin <111556881+xyvlin@users.noreply.github.com>
2025-02-17 19:56:15 +0800

    Add files via upload

--- ransac/ransac.py
+++ ransac/ransac.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1D4K1g0mlAHl3NFhdwo11DHXXo-3B3Gvv
 """
 
-from sklearn.metrics import mean_absolute_error as mae
+from sklearn.metrics import median_absolute_error as mae
 from copy import deepcopy
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -35,7 +35,7 @@ class Ransac:
     if self.min_sample > X.shape[0]:
         self.min_sample = X.shape[0]
     sampleSize = X.shape[0]
-    samples = np.arange(sampleSize)
+    samples = np.arange(self.min_sample)
 
     for n in range(1,self.max_trials+1):
       # 1. select a random number of examples to be inliers and fit the model
@@ -99,4 +99,47 @@ plt.show()
 
 print('Slope: %.3f' % ransac.bestModel.coef_[0])
 print('Intercept: %.3f' % ransac.bestModel.intercept_)
+print('MAD:',mae(y,ransac.predict(X)))
 
+lr = LinearRegression()
+lr.fit(X,y)
+plt.scatter(X, y, c='steelblue', edgecolor='white', marker='o')
+line_X = np.arange(3, 10, 1)
+line_y_lr = lr.predict(line_X[:, np.newaxis])
+plt.plot(line_X, line_y_lr, color='black', lw=2)
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+print('Slope: %.3f' % lr.coef_[0])
+print('Intercept: %.3f' % lr.intercept_)
+print('MAD:',mae(y,lr.predict(X)))
+
+from sklearn.linear_model import RANSACRegressor
+
+skransac = RANSACRegressor(LinearRegression(),
+              max_trials=100,
+              min_samples=50,
+              residual_threshold=5.0,
+              random_state=0)
+skransac.fit(X, y)
+
+inlier_mask = skransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+
+line_X = np.arange(3, 10, 1)
+line_y_ransac = skransac.predict(line_X[:, np.newaxis])
+
+plt.scatter(X[inlier_mask], y[inlier_mask],
+            c='steelblue', edgecolor='white',
+            marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask],
+            c='limegreen', edgecolor='white',
+            marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='black', lw=2)
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper left')
+plt.show()
+
+print('Slope: %.3f' % skransac.estimator_.coef_[0])
+print('Intercept: %.3f' % skransac.estimator_.intercept_)
+print('MAD:', mae(y, skransac.predict(X)))
